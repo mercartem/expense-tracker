b4d385613a4aa92053f1beb0fb6314f950daf52a
Artemiy Gudz <mercartem@mail.ru>
2023-01-31 01:41:33 +0300

    init: add folders according to fsd structure

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
-import App from './App';
+import App from './app/App';
 
 const rootElement = document.getElementById('root') as HTMLElement;
 const root = ReactDOM.createRoot(rootElement);
