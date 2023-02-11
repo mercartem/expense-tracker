a41cb5afa9cf6260ca44634b6cbc6059823716fa
marerma <maria.kadantseva@gmail.com>
2023-02-11 18:30:32 +0300

    feat: add balance context

---
+++ frontend/src/app/context/BalanceContext.tsx
@@ -0,0 +1,20 @@
+import { createContext } from 'react';
+import getBalance from '../../entities/Balance/api/getBalance';
+import { getToken } from '../../shared/utils/utils';
+
+interface IBalanceContext {
+  balance: string;
+  updateBalance: () => Promise<void>;
+}
+
+const defaultBalanceState = {
+  balance: '',
+  async updateBalance() {
+    const token = getToken() as string;
+    const currentBalance = await getBalance(token);
+  }
+  }
+
+const BalanceContext = createContext<IBalanceContext>(defaultBalanceState);
+
+export default BalanceContext;
