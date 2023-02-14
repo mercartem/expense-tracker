7b1e2b4e09424537e0329c4e2f05cf9080e64296
Artemiy Gudz <mercartem@mail.ru>
2023-02-14 17:38:36 +0300

    feat: add changing avatar

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,21 +2,6 @@ import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './app/App';
-// import createTransaction from './entities/Transaction/api/createTransaction';
-// import { getToken } from './shared/utils/utils';
-
-// createTransaction(
-//   {
-//     category: 'Transporation',
-//     description: 'бензин',
-//     amount: 3000,
-//     paymentMode: 'cash',
-//     transactionType: 'income',
-//     date: '2022-09-04T21:00:00.000Z',
-//     time: '00:00',
-//   },
-//   getToken() as string,
-// ).then((res) => console.log(res));
 
 const rootElement = document.getElementById('root') as HTMLElement;
 const root = ReactDOM.createRoot(rootElement);
