1b4c6878e4c0e5e8a781c2c0b8a334fdae0d7318
Artemiy Gudz <mercartem@mail.ru>
2023-02-21 19:26:16 +0300

    feat: add message about add avatar

--- frontend/src/widgets/AddAvatar/ui/AddAvatar.tsx
+++ frontend/src/widgets/AddAvatar/ui/AddAvatar.tsx
@@ -1,6 +1,6 @@
 import { useTranslation } from 'react-i18next';
 import { Button } from '@mui/material';
-import { ChangeEvent, useContext } from 'react';
+import { ChangeEvent, useContext, useState } from 'react';
 import { AvatarContext } from '../../../app/context/AvatarContext';
 import createAvatarUser from '../../../entities/User/api/createAvatarUser';
 import { getId, getToken } from '../../../shared/utils/utils';
@@ -8,6 +8,7 @@ import { getId, getToken } from '../../../shared/utils/utils';
 function AddAvatar() {
   const { t } = useTranslation();
   const { updateAvatar } = useContext(AvatarContext);
+  const [success, setSuccess] = useState('');
 
   const handleFileSelect = async (event: ChangeEvent<HTMLInputElement>) => {
     const formData = new FormData();
@@ -17,6 +18,7 @@ function AddAvatar() {
     if (file) {
       formData.append('image', file, `${userId}.jpg`);
       createAvatarUser(token, formData);
+      setSuccess(t('avatarChange') as string);
       setTimeout(() => updateAvatar(), 1000);
     }
   };
@@ -31,6 +33,11 @@ function AddAvatar() {
         {t('avatarTitle')}
         <input hidden accept='image/*' multiple type='file' onChange={handleFileSelect} />
       </Button>
+      <p
+        style={{ color: 'rgb(44, 148, 44)', paddingLeft: '5px', marginTop: '5px', height: '20px' }}
+      >
+        {success}
+      </p>
     </div>
   );
 }
